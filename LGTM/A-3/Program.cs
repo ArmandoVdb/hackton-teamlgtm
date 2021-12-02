3c5ce1983a3c0e22d3e8f1af80690950e2181530
Lars Houtman <lars.houtman@student.kdg.be>
2021-12-02 15:14:58 +0100

    solved all conflicts

--- LGTM/A-3/Program.cs
+++ LGTM/A-3/Program.cs
@@ -1,5 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using A_3;
+
+static class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = LGTM.Our.Client();
+        var sampleUrl = "/api/path/1/hard/Sample";
+        var sampleResponse = await client.GetFromJsonAsync<Response>(sampleUrl);
+        Console.WriteLine(sampleResponse);
+
+    }
+
+}
